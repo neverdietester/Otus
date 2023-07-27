6d1dfbc817379a4db2f4fa77a9e34d21f9274502
Olga Novikova <Olga.Novikova@kaspersky.com>
2023-07-27 14:15:46 +0300

    Homework 4 ver 3

--- Homework4/Program.cs
+++ Homework4/Program.cs
@@ -12,9 +12,9 @@ namespace Homework4
             var s = new Stack("a", "b", "c");
             // size = 3, Top = 'c'
             Console.WriteLine($"size = {s.Size}, Top = '{s.Top}'");
-            s.Pop();
+            var deleted = s.Pop();
             // Извлек верхний элемент 'c' Size = 2
-            Console.WriteLine($"Извлек верхний элемент, Size = {s.Size}");
+            Console.WriteLine($"Извлек верхний элемент '{deleted}' Size = {s.Size}");
             s.Add("d");
             // size = 3, Top = 'd'
             Console.WriteLine($"size = {s.Size}, Top = '{s.Top}'");
@@ -24,52 +24,6 @@ namespace Homework4
             // size = 0, Top = null
             Console.WriteLine($"size = {s.Size}, Top = {(s.Top == null ? "null" : s.Top)}");
             s.Pop();
-        }
-
-        class Stack
-        {
-            private List<string> lines;
-
-            public Stack(params string[] input)
-            {
-                lines = input.ToList();
-            }
-
-            public void Add(string backstitch)
-            {
-                lines.Add(backstitch);
-
-            }
-
-            public void Pop()
-            {
-                try
-                {
-                    if (lines.Count >= 1)
-                    {
-                        lines.RemoveAt(lines.Count - 1);
-                    }
-                }
-                catch (IndexOutOfRangeException)
-                {
-                    throw new Exception("Стек пустой");
-                }
-            }
-
-            public int Size => lines.Count;
-
-            public string Top
-            {
-                get
-                {
-                    if (lines.Count == 0)
-                    {
-                        return null;
-                    }
-                    var lastItem = lines.Last();
-                    return lastItem;
-                }
-            }
         }
     }
 }
---
+++ Homework4/Stack.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4
+{
+    class Stack
+    {
+        private List<string> _lines;
+
+        public Stack(params string[] input)
+        {
+            _lines = input.ToList();
+        }
+
+        public void Add(string backstitch)
+        {
+            _lines.Add(backstitch);
+
+        }
+        public string Pop()
+        {
+            if (_lines.Count == 0)
+            {
+                return null;
+            }
+            var lastItem = _lines.Last();
+            try
+            {
+                if (_lines.Count >= 1)
+                {
+                    _lines.RemoveAt(_lines.Count - 1);
+                }
+            }
+            catch (IndexOutOfRangeException)
+            {
+            }
+
+            return lastItem;
+        }
+        public int Size => _lines.Count;
+
+        public string Top
+        {
+            get
+            {
+                if (_lines.Count == 0)
+                {
+                    return null;
+                }
+                var lastItem = _lines.Last();
+                return lastItem;
+            }
+        }
+    }
+}
