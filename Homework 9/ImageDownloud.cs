941ed9b05b52ab0088a9a6bff0a3e00917a88e95
Olga Novikova <Olga.Novikova@kaspersky.com>
2023-09-11 20:08:03 +0300

    Homework 9 ver 2

---
+++ Homework 9/ImageDownloud.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using static Homework_9.ImageDownloud;
+
+namespace Homework_9
+{
+    public class ImageDownloud
+    {
+        public delegate void ImageStarted();
+        public delegate void ImageCompleted();
+
+        public event ImageStarted onImageStarted;
+        public event ImageCompleted onImageCompleted;
+
+        public void Download()
+        {
+            // Откуда будем качать
+            string remoteUri = "https://stsci-opo.org/STScI-01H7TESNNSX9CH4EQ29C919C4M.png";
+            // Как назовем файл на диске
+            string fileName = "bigimage.png";
+
+            // Качаем картинку в текущую директорию
+            var myWebClient = new WebClient();
+            onImageStarted?.Invoke();
+
+            Console.WriteLine("Качаю \"{0}\" из \"{1}\" .......\n\n", fileName, remoteUri);
+
+            myWebClient.DownloadFile(remoteUri, fileName);
+            onImageCompleted?.Invoke();
+        }
+    }
+}
---
+++ Homework 9/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+using Homework_9;
+
+class Program
+{
+    public static void Main()
+    {
+        void SubscribeStart()
+        {
+            Console.WriteLine("Скачивание файла началось");
+        }
+        void SubscribeStop()
+        {
+            Console.WriteLine("Скачивание файла закончилось");
+        }
+
+        ImageDownloud imageDownloud = new ImageDownloud();
+        imageDownloud.onImageStarted += SubscribeStart;
+        imageDownloud.onImageCompleted += SubscribeStop;
+        imageDownloud.Download();
+       
+
+        Console.WriteLine("Нажмите любую клавишу");
+        Console.ReadLine();
+
+    }
+}
