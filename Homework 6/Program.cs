afea2e1a7f382bad26189e4df5277ae7551c6e92
Olga Novikova <Olga.Novikova@kaspersky.com>
2023-10-03 12:38:16 +0300

    Homework all_v2

--- Homework 6/Program.cs
+++ Homework 6/Program.cs
@@ -9,47 +9,47 @@ namespace Homework_6
     {
         static void Main(string[] args)
         {
-            //    Console.WriteLine("Программа 1");
+            Console.WriteLine("Программа 1");
 
-            //    var Planeta1 = new
-            //    {
-            //        Name = "Venus",
-            //        SerialNumber = 2,
-            //        LengthOfEquator = 38025,
-            //        previousPlanet = (object)null
-            //    };
-            //    var Planeta2 = new
-            //    {
-            //        Name = "Earth",
-            //        SerialNumber = 3,
-            //        LengthOfEquator = 40075,
-            //        PreviousPlanet = Planeta1
-            //    };
-            //    var Planeta3 = new
-            //    {
-            //        Name = "Mars",
-            //        SerialNumber = 4,
-            //        LengthOfEquator = 21344,
-            //        PreviousPlanet = Planeta2
-            //    };
+            var Planeta1 = new
+            {
+                Name = "Venus",
+                SerialNumber = 2,
+                LengthOfEquator = 38025,
+                previousPlanet = (object)null
+            };
+            var Planeta2 = new
+            {
+                Name = "Earth",
+                SerialNumber = 3,
+                LengthOfEquator = 40075,
+                PreviousPlanet = Planeta1
+            };
+            var Planeta3 = new
+            {
+                Name = "Mars",
+                SerialNumber = 4,
+                LengthOfEquator = 21344,
+                PreviousPlanet = Planeta2
+            };
 
-            //    Console.WriteLine($"Solar system object:{Planeta1}");
-            //    Console.WriteLine($"{Planeta1.Name} & {Planeta1.Name} : { Planeta1.Equals(Planeta1)}");
-            //    Console.WriteLine($"Solar system object:{Planeta2}");
-            //    Console.WriteLine($"{Planeta1.Name} & {Planeta2.Name} : {Planeta2.Equals(Planeta1)}");
-            //    Console.WriteLine($"Solar system object:{Planeta3}");
-            //    Console.WriteLine($"{Planeta1.Name} & {Planeta3.Name} : {Planeta2.Equals(Planeta1)}");
+            Console.WriteLine($"Solar system object:{Planeta1}");
+            Console.WriteLine($"{Planeta1.Name} & {Planeta1.Name} : { Planeta1.Equals(Planeta1)}");
+            Console.WriteLine($"Solar system object:{Planeta2}");
+            Console.WriteLine($"{Planeta1.Name} & {Planeta2.Name} : {Planeta2.Equals(Planeta1)}");
+            Console.WriteLine($"Solar system object:{Planeta3}");
+            Console.WriteLine($"{Planeta1.Name} & {Planeta3.Name} : {Planeta2.Equals(Planeta1)}");
 
-            //    Console.WriteLine();
-            //    Console.WriteLine("Программа 2");
+            Console.WriteLine();
+            Console.WriteLine("Программа 2");
 
 
-            //    PlanetList findPlanet = new PlanetList();
-            //    findPlanet.GetPlanet("Earth");
-            //    findPlanet.GetPlanet("Limoniya");
-            //    findPlanet.GetPlanet("Mars");
-            //    findPlanet.GetPlanet("Limoniya");
-            //    findPlanet.GetPlanet("Mars");
+            PlanetList findPlanet = new PlanetList();
+            findPlanet.GetPlanet("Earth");
+            findPlanet.GetPlanet("Limoniya");
+            findPlanet.GetPlanet("Mars");
+            findPlanet.GetPlanet("Limoniya");
+            findPlanet.GetPlanet("Mars");
 
             Console.WriteLine();
             Console.WriteLine("Программа 3");
